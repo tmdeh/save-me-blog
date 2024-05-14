8add92b99613bfbbc6efc2daa19f50d668886a76
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-05-14 16:41:33 +0900

    init

---
+++ src/main/java/com/delivery/save_me_seungdo_blog/SaveMeSeungdoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.delivery.save_me_seungdo_blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveMeSeungdoBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaveMeSeungdoBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/delivery/save_me_seungdo_blog/SaveMeSeungdoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.delivery.save_me_seungdo_blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveMeSeungdoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
