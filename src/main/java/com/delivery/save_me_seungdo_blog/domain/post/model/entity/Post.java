89674201bd3f51b91dd84cf17941805c836c059d
윾승도 <66948129+tmdeh@users.noreply.github.com>
2024-05-17 16:27:22 +0900

    fix : 예외처리 수정

--- src/main/java/com/delivery/save_me_seungdo_blog/domain/post/model/entity/Post.java
+++ src/main/java/com/delivery/save_me_seungdo_blog/domain/post/model/entity/Post.java
@@ -1,26 +1,18 @@
 package com.delivery.save_me_seungdo_blog.domain.post.model.entity;
 
-import com.delivery.save_me_seungdo_blog.domain.auth.model.entity.User;
 import com.delivery.save_me_seungdo_blog.global.model.entity.TimeStamp;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.time.Instant;
 import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "post")
 public class Post extends TimeStamp {
@@ -40,9 +32,9 @@ public class Post extends TimeStamp {
     @Column(name = "content", nullable = false)
     private String content;
 
-    @NotNull
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
-    private User user;
+//    @NotNull
+//    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+//    @JoinColumn(name = "user_id", nullable = false)
+//    private User user;
 
 }
--- src/main/java/com/delivery/save_me_seungdo_blog/global/model/response/Api.java
+++ src/main/java/com/delivery/save_me_seungdo_blog/global/model/response/Api.java
@@ -1,13 +1,47 @@
 package com.delivery.save_me_seungdo_blog.global.model.response;
 
+import com.delivery.save_me_seungdo_blog.global.exception.CustomApiException;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
-@Builder
+@Getter
 @AllArgsConstructor
 public class Api<T> {
 
     private Result result;
 
     private T body;
+
+    // 커스텀 응답
+    public static <T> Api<T> SUCCESS(HttpStatus status, T body) {
+        Result result = new Result(status.value(), status.name());
+        return new Api<>(result, body);
+    }
+
+    // body가 있는 성공 응답
+    public static <T> Api<T> OK(T body) {
+        Result result = new Result(HttpStatus.OK.value(), HttpStatus.OK.name());
+        return new Api<>(result, body);
+    }
+
+    // body가 없는 성공 응답
+    public static Api<Object> OK() {
+        Result result = new Result(HttpStatus.OK.value(), "OK");
+        return new Api<>(result, null);
+    }
+
+
+    // 예외 응답
+    public static Api<Object> EXCEPTION(CustomApiException exception) {
+        Result result = new Result(exception.getStatusCode().value(), exception.getMessage());
+        return new Api<>(result, null);
+    }
+
+    // 서버 에러 응답
+    public static Api<Object> ERROR(Exception e) {
+        Result result = new Result(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+        return new Api<>(result, null);
+    }
+
 }
