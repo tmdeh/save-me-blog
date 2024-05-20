c1de695b7f734a2dad42629f0f070965916b92e8
윾승도 <yus174113@gmail.com>
2024-05-20 21:54:18 +0900

    refactor : 에러 문구 변경

--- src/main/java/com/fc/save_me_seungdo_blog/global/exception/code/AuthErrorCode.java
+++ src/main/java/com/fc/save_me_seungdo_blog/global/exception/code/AuthErrorCode.java
@@ -11,7 +11,7 @@ public enum AuthErrorCode implements ErrorCode {
     NOT_FOUND(HttpStatus.BAD_REQUEST, "해당 사용자는 없습니다."),
     EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "토큰이 만료됐습니다."),
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "잘못된 토큰입니다."),
-    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "Unauthorized"),
+    UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
     ;
 
     private final HttpStatus status;
